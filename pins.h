de7069ed7535eca6055e1bd95c5eda8754671b41
Paulus Järvinen <paulus.jarvinen@gmail.com>
2026-05-08 16:42:11 +0300

    Add files via upload

---
+++ pins.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define PIN_TRIGGER 12
+#define PIN_RELOAD 13
+#define PIN_START 14
+
+#define PIN_IR_TX 6
+#define PIN_IR_RX 11
+#define PIN_RUMBLE_MOTOR 36
+#define PIN_BUZZER 35
+#define DEBUG_SW 0
+
+#define STATUS_LED1 45
+#define STATUS_LED2 48
+#define STATUS_LED3 47
+
+#define SCL_MPU 1
+#define SDA_MPU 2
+
+#define SDA_OLED 4
+#define SCL_OLED 5
