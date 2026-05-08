9657a1c8aed0b95b1814df937a7454627864f0d1
Paulus Järvinen <paulus.jarvinen@gmail.com>
2026-05-08 17:57:48 +0300

    Main loop updates

--- laser_tag.ino
+++ laser_tag.ino
@@ -2,6 +2,9 @@
 #include "defs.h"
 #include "display.h"
 #include "gyro.h"
+#include "game.h"
+
+ty_game_state state = ST_INIT;
 
 void setup () {
     Serial.begin(115200);
@@ -17,6 +20,38 @@ void loop() {
 
     // mqtt loop needed here!!
 
+    swtich (state) {
 
+        case ST_INIT:
+            ST = ST_WAIT_MODE;
+            break;
 
+        case ST_WAIT_MODE:
+            if (debug_mode_active()) state = ST_DEBUG;
+            else state = ST_WAIT_START;
+            break;
+
+        case ST_WAIT_START:
+            if (start_button_pressed()) {
+                game_start();
+                state = ST_PLAY;
+            }
+            break;
+
+        case ST_PLAY:
+            game_update();
+            if (game_over()) {
+                state = ST_GAME_OVER;
+            }
+            break;
+
+        case ST_GAME_OVER;
+            game_over();
+            state = ST_WAIT_START;
+            break;
+
+        case STATE_DEBUG_
+            debug_mode();
+            break;
+    }
 }
